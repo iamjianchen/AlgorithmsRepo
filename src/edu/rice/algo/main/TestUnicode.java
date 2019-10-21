88e6af35a50db4fa8a356b7affdb73ce4408379e
iamjianchen <jianchen319@gmail.com>
2019-10-20 20:31:59 -0500

    Unicode Update

---
+++ src/edu/rice/algo/main/TestUnicode.java
@@ -0,0 +1,12 @@
+package edu.rice.algo.main;
+
+public class TestUnicode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 2000 ; i++) {
+			System.out.println((char) i + ":" + i);
+		}
+	}
+
+}
