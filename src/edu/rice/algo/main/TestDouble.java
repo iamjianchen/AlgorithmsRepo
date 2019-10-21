c3dc2616709b4554b0150988333beb4dc84b125d
iamjianchen <jianchen319@gmail.com>
2019-10-20 19:41:28 -0500

    Update double type number problem

---
+++ src/edu/rice/algo/main/TestDouble.java
@@ -0,0 +1,12 @@
+package edu.rice.algo.main;
+
+public class TestDouble {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double doubleNum = 2.6;
+		double result = doubleNum - 2;
+		System.out.println(result);
+	}
+
+}
